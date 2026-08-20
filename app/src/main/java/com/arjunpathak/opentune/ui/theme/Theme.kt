f5695dc601ebe61f892a5d46a66881444fe28145
Arjun <310191442+ArjunPathak1025@users.noreply.github.com>
2026-08-20 22:21:29 +0530

    feat(settings): support dynamic light and dark theme

--- app/src/main/java/com/arjunpathak/opentune/ui/theme/Theme.kt
+++ app/src/main/java/com/arjunpathak/opentune/ui/theme/Theme.kt
@@ -2,14 +2,19 @@ package com.arjunpathak.opentune.ui.theme
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
-private val OpenTuneColors = darkColorScheme()
+private val OpenTuneDarkColors = darkColorScheme()
+private val OpenTuneLightColors = lightColorScheme()
 
 @Composable
-fun OpenTuneTheme(content: @Composable () -> Unit) {
+fun OpenTuneTheme(
+    darkTheme: Boolean = true,
+    content: @Composable () -> Unit
+) {
     MaterialTheme(
-        colorScheme = OpenTuneColors,
+        colorScheme = if (darkTheme) OpenTuneDarkColors else OpenTuneLightColors,
         content = content
     )
 }
